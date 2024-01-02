87f808cae870a54bd327a2e61079a0e6bbc27e15
ZiadMotagaly <ziad.motagaly@gmail.com>
2024-01-02 01:07:43 +0100

    Create helpers.py

---
+++ t2_diffusion_model/helpers.py
@@ -0,0 +1,95 @@
+def exists(x):
+    return x is not None
+
+def default(val, d):
+    if exists(val):
+        return val
+    return d() if isfunction(d) else d
+
+# We will use this to convert timestamps to time encodings
+class SinusoidalPosEmb(Layer):
+    def __init__(self, dim, max_positions=10000):
+        super(SinusoidalPosEmb, self).__init__()
+        self.dim = dim
+        self.max_positions = max_positions
+
+    def call(self, x, training=True):
+        x = tf.cast(x, tf.float32)
+        half_dim = self.dim // 2
+        emb = math.log(self.max_positions) / (half_dim - 1)
+        emb = tf.exp(tf.range(half_dim, dtype=tf.float32) * -emb)
+        emb = x[:, None] * emb[None, :]
+
+        emb = tf.concat([tf.sin(emb), tf.cos(emb)], axis=-1)
+
+        return emb
+        
+# small helper modules
+class Identity(Layer):
+    def __init__(self):
+        super(Identity, self).__init__()
+
+    def call(self, x, training=True):
+        return tf.identity(x)
+
+
+class Residual(Layer):
+    def __init__(self, fn):
+        super(Residual, self).__init__()
+        self.fn = fn
+
+    def call(self, x, training=True):
+        return self.fn(x, training=training) + x
+
+def Upsample(dim):
+    return nn.Conv2DTranspose(filters=dim, kernel_size=4, strides=2, padding='SAME')
+
+def Downsample(dim):
+    return nn.Conv2D(filters=dim, kernel_size=4, strides=2, padding='SAME')
+
+class LayerNorm(Layer):
+    def __init__(self, dim, eps=1e-5, **kwargs):
+        super(LayerNorm, self).__init__(**kwargs)
+        self.eps = eps
+
+        self.g = tf.Variable(tf.ones([1, 1, 1, dim]))
+        self.b = tf.Variable(tf.zeros([1, 1, 1, dim]))
+
+    def call(self, x, training=True):
+        var = tf.math.reduce_variance(x, axis=-1, keepdims=True)
+        mean = tf.reduce_mean(x, axis=-1, keepdims=True)
+
+        x = (x - mean) / tf.sqrt((var + self.eps)) * self.g + self.b
+        return x
+
+class PreNorm(Layer):
+    def __init__(self, dim, fn):
+        super(PreNorm, self).__init__()
+        self.fn = fn
+        self.norm = LayerNorm(dim)
+
+    def call(self, x, training=True):
+        x = self.norm(x)
+        return self.fn(x)
+
+class SiLU(Layer):
+    def __init__(self):
+        super(SiLU, self).__init__()
+
+    def call(self, x, training=True):
+        return x * tf.nn.sigmoid(x)
+
+def gelu(x, approximate=False):
+    if approximate:
+        coeff = tf.cast(0.044715, x.dtype)
+        return 0.5 * x * (1.0 + tf.tanh(0.7978845608028654 * (x + coeff * tf.pow(x, 3))))
+    else:
+        return 0.5 * x * (1.0 + tf.math.erf(x / tf.cast(1.4142135623730951, x.dtype)))
+
+class GELU(Layer):
+    def __init__(self, approximate=False):
+        super(GELU, self).__init__()
+        self.approximate = approximate
+
+    def call(self, x, training=True):
+        return gelu(x, self.approximate)
